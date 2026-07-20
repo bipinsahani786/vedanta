5584c5aef12d3e9168bfb9da12f3ed5f05f8122d
backend-sws <backend.sws@gmail.com>
2026-07-20 13:00:07 +0530

    feat: implement admin job management and automated candidate notifications for job matches

--- app/Mail/CandidateJobMatchNotification.php
+++ app/Mail/CandidateJobMatchNotification.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class CandidateJobMatchNotification extends Mailable
+class CandidateJobMatchNotification extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
--- resources/views/emails/candidate/job_match.blade.php
+++ resources/views/emails/candidate/job_match.blade.php
@@ -14,6 +14,15 @@ A new job post has been approved that matches **{{ $matchScore }}%** of your pro
 View Job & Apply
 </x-mail::button>
 
+<x-mail::panel>
+**Get In Touch**  
+Career Point Building, 2nd floor,  
+Patna, 800001, Bihar
+
+**Email:** info@vedantaplacementagency.in  
+**Phone:** +91-7070938975
+</x-mail::panel>
+
 Best regards,<br>
-{{ config('app.name') }}
+**Vedanta Placement Agency**
 </x-mail::message>
