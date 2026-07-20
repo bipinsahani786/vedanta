83ab4b4fe9eb6e00a0eec7c03f127cbc651ab3ef
CHITranjan7647 <chitranjan4102006@gmail.com>
2026-07-20 11:43:55 +0530

    feat: implement candidate CRM dashboard and update database seeder with user management logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             DummyDataSeeder::class,
+            StateFixSeeder::class,
         ]);
     }
 }
